8d96812f1ad0016bda67b8c5467103ae72e733e2
Dmitriy Ermakov <dmitriy.ermakov@sky-tech.co>
2016-06-16 17:01:24 +0300

    * Working on SnapshotService.

--- Jupiter1.Network.Server/Services/SnapshotService/SnapshotService.cs
+++ Jupiter1.Network.Server/Services/SnapshotService/SnapshotService.cs
@@ -29,8 +29,66 @@ namespace Jupiter1.Network.Server.Services.SnapshotService
             _serverChannelService = serverChannelService;
         }
 
+        private void BuildSnapshotToClient(Client client)
+        {
+        }
+
+        private void UpdateServerCommandsToClient()
+        {
+        }
+
+        private void WriteSnapshotToClient()
+        {
+        }
+
+        private void WriteDownloadToClient()
+        {
+        }
+
+        private void SendMessageToClient()
+        {
+        }
+
         private void SendSnapshotToClient(Client client)
         {
+            //byte msg_buf[MAX_MSGLEN];
+            //msg_t msg;
+
+            //// build the snapshot
+            //SV_BuildClientSnapshot(client);
+
+            //// bots need to have their snapshots build, but
+            //// the query them directly without needing to be sent
+            //if (client->gentity && client->gentity->r.svFlags & SVF_BOT)
+            //{
+            //    return;
+            //}
+
+            //MSG_Init(&msg, msg_buf, sizeof(msg_buf));
+            //msg.allowoverflow = qtrue;
+
+            //// NOTE, MRE: all server->client messages now acknowledge
+            //// let the client know which reliable clientCommands we have received
+            //MSG_WriteLong(&msg, client->lastClientCommand);
+
+            //// (re)send any reliable server commands
+            //SV_UpdateServerCommandsToClient(client, &msg);
+
+            //// send over all the relevant entityState_t
+            //// and the playerState_t
+            //SV_WriteSnapshotToClient(client, &msg);
+
+            //// Add any download data if the client is downloading
+            //SV_WriteDownloadToClient(client, &msg);
+
+            //// check for overflow
+            //if (msg.overflowed)
+            //{
+            //    Com_Printf("WARNING: msg overflowed for %s\n", client->name);
+            //    MSG_Clear(&msg);
+            //}
+
+            //SV_SendMessageToClient(&msg, client);
         }
 
         // Return the number of msec a given size message is supposed to take to clear, based on the current rate.
