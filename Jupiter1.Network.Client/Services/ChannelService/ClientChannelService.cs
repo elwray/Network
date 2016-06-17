900b8e7676975c62a65a76be6f63ca8a6fe6131c
Dmitriy Ermakov <dmitriy.ermakov@sky-tech.co>
2016-06-17 18:01:55 +0300

    * Work in progress.

---
+++ Jupiter1.Network.Client/Services/ChannelService/ClientChannelService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Jupiter1.Network.Common.Services.ChannelService;
+using Jupiter1.Network.Common.Structures;
+
+namespace Jupiter1.Network.Client.Services.ChannelService
+{
+    internal sealed class ClientChannelService : IClientChannelService
+    {
+        private readonly IChannelService _channelService;
+
+        public ClientChannelService(IChannelService channelService)
+        {
+            if (channelService == null)
+                throw new ArgumentNullException(nameof(channelService));
+
+            _channelService = channelService;
+        }
+
+        #region IClientChannelService
+        public void Transmit(NetworkChannel channel, Message message)
+        {
+            if (channel == null)
+                throw new ArgumentNullException(nameof(channel));
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+        }
+
+        public void TransmitNext(NetworkChannel channel)
+        {
+            if (channel == null)
+                throw new ArgumentNullException(nameof(channel));
+
+            _channelService.TransmitNext(channel);
+        }
+
+        public bool Process(NetworkChannel channel, Message message)
+        {
+            if (channel == null)
+                throw new ArgumentNullException(nameof(channel));
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ Jupiter1.Network.Client/Services/ChannelService/IClientChannelService.cs
@@ -0,0 +1,11 @@
+﻿using Jupiter1.Network.Common.Structures;
+
+namespace Jupiter1.Network.Client.Services.ChannelService
+{
+    internal interface IClientChannelService
+    {
+        void Transmit(NetworkChannel channel, Message message);
+        void TransmitNext(NetworkChannel channel);
+        bool Process(NetworkChannel channel, Message message);
+    }
+}
